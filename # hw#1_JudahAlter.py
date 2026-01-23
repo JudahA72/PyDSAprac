d15ef74c1ebae7b694330b7c2355041f95228db4
JudahA72 <Judahalter@gmail.com>
2026-01-22 23:54:14 -0500

    Completed assignment, 5 problems using basic data types and looping/conditional structures

--- # hw#1_JudahAlter.py
+++ # hw#1_JudahAlter.py
@@ -3,8 +3,8 @@ CIS-4930 Introduction to Python, Spring 2026
 Homework : 1
 Student Name: Judah Alter
 Student ID: JAA22H
-Section: 
-Submission Date: [MM-DD-YYYY]
+Section:3 
+Submission Date: [01-22-2026]
 """
 
 # Problem 1 Planetary Cargo Weight Checker
@@ -43,10 +43,157 @@ for n in range(items):
     
 # Problem 2 Astronaut Training Fatigue Tracker
 
-Day = int(input("Enter the number of training hours tracked in a day:"))
+hours = int(input("Enter the number of training hours tracked in a day: "))
 
-for n in range(Day):
-    fatigue = int(input("Enter your fatige level for the hour (0-10):"))
+maxFatigue = -1
+DangerHours = 0
+increase = 0
+previous = None
+
+for n in range(hours):
+    fatigue = int(input(f"Enter your fatigue level for hour {n + 1} (0-10): "))
+
+    if fatigue > maxFatigue:
+        maxFatigue = fatigue
+
+    if fatigue > 8:
+        DangerHours += 1
+
+    if previous is not None and fatigue > previous:
+        increase += 1
+
+    previous = fatigue
+
+if DangerHours > 8:
+    classification = "HIGH RISK - Medical review required"
+elif increase > 3:
+    classification = "MODERATE RISK - Extra rest recommended"
+else:
+    classification = "NORMAL - Proceed"
+
+print(f"Hours tracked: {hours}, Max fatigue: {maxFatigue}, Dangerous hours: {DangerHours}, Increases: {increase}")
+print(f"Training day classification: {classification}")
+
+
+# Problem 3 Interactive Dungeon Door Code Puzzle
+
+
+energy = 0
+
+while True:
+    energyEnter = float(input("Enter energy to add to the door (negative to remove):"))
+    energy += energyEnter   
+
+    exit = input("Write quit if you want to exit(anything else to continue):").lower()
+    if exit == "quit":
+        print()
+        break
+    if energy > 50:
+        print(f"Door opens with energy {energy}")
+        break
+    elif energy < 0:
+        energy = 0
+        print("Door resets to 0.")
+    else:
+        print("Keep trying to open the door.")
+
+
+
+# Problem 4 Librarian's Overdue Book Fine Calculator
+
+BookType = input("Enter the type of book (novel, textbook, childrens):").lower()
+DaysOverdue = int(input("Enter the number of days the book is overdue:"))
+BorrowersAge = int(input("Enter the age of the borrower:"))
+
+InitialFine = 0
+TotalFine = 0
+DiscountAmount = 0
+OverdueFee = 0
+
+if BookType not in ["novel", "textbook", "childrens"]:
+    print("Invalid book type using novel pricing.")
+    BookType = "novel"
+
+if BookType == "novel":
+    OverdueFee = 5
+    InitialFine += DaysOverdue * 0.25
+    TotalFine = InitialFine
+    if BorrowersAge < 12:
+        DiscountAmount = InitialFine * 0.50
+        TotalFine = InitialFine - DiscountAmount
+    elif InitialFine < 0:
+        InitialFine = 0
+        print("Total fine set to 0 since it was negative.")
+    elif DaysOverdue > 30:
+        TotalFine += OverdueFee
+print(f"Books {BookType} overdue {DaysOverdue}, Borrower age {BorrowersAge}")
+print(f"Base fine: ${InitialFine}")
+print(f"Youth discount applied: -${DiscountAmount}")
+print(f"Long overdue fee applied: +${OverdueFee if DaysOverdue > 30 else 0}")
+print(f"Total due: ${TotalFine}")
+
+if BookType == "textbook":
+    OverdueFee = 5
+    InitialFine += DaysOverdue * 0.5
+    TotalFine = InitialFine
+    if BorrowersAge < 12:
+        DiscountAmount = InitialFine * 0.50
+        TotalFine = InitialFine - DiscountAmount
+    elif InitialFine < 0:
+            InitialFine = 0
+            print("Total fine set to 0 since it was negative.")
+    elif DaysOverdue > 30:
+            TotalFine += OverdueFee
+print(f"Books {BookType} overdue {DaysOverdue}, Borrower age {BorrowersAge}")
+print(f"Base fine: ${InitialFine}")
+print(f"Youth discount applied: -${DiscountAmount}")
+print(f"Long overdue fee applied: +${OverdueFee if DaysOverdue > 30 else 0}")
+print(f"Total due: ${TotalFine}")
+
+if BookType == "childrens":
+    OverdueFee = 5
+    InitialFine += DaysOverdue * 0.125
+    TotalFine = InitialFine
+    if BorrowersAge < 12:
+        DiscountAmount = InitialFine * 0.50
+        TotalFine = InitialFine - DiscountAmount
+    elif InitialFine < 0:
+        InitialFine = 0
+        print("Total fine set to 0 since it was negative.")
+    elif DaysOverdue > 30:
+        TotalFine += OverdueFee
     
+print(f"Books {BookType} overdue {DaysOverdue}, Borrower age {BorrowersAge}")
+print(f"Base fine: ${InitialFine}")
+print(f"Youth discount applied: -${DiscountAmount}")
+print(f"Long overdue fee applied: +${OverdueFee if DaysOverdue > 30 else 0}")
+print(f"Total due: ${TotalFine}")
+    
+
+
+#Problem 5 Guess the number with Adaptive Hunts
+
+secret = 72
+NumberofGuesses = 0
+
+while True:
+    guess = int(input("Enter your guess: "))
+    NumberofGuesses += 1
+    difference = abs(secret - guess)
+
+    if guess == secret:
+        print(f"Congratulations! Secret number is {secret}. You guessed the secret {secret} number in {NumberofGuesses} guesses.")
+        break
+    elif difference <= 5:
+        print(f"Very Close. {'Too High.' if secret < guess else 'Too Low.'}")
+    elif difference <=15 and difference > 5:
+        print(f"Warm. {'Too High.' if secret < guess else 'Too Low.'}")
+    else:
+        print(f"Cold. {'Too High.' if secret < guess else 'Too Low.'}")
+
+
+
+
+
 
 
